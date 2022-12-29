6d76e13091fa56e9b0bf725fb653234502f0801a
Mnogokica <alisa.suhoveiko@gmail.com>
2022-12-29 13:22:29 +0200

    add homework

---
+++ md_1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(){
+cout<<"Runci, runci, kaki, kaki,"<<endl;
+cout<<"Iesim abi pelu kert."<<endl;
+cout<<"Pasusim berniniem"<<endl;
+cout<<"Pelu adas kazocinu."<<endl<<endl<<endl;
+cout<<"                _|\\_"<<endl;
+cout<<"               \\` ..\\"<<endl;
+cout<<"          __,.-\" =__Y="<<endl;
+cout<<"        .\"        )"<<endl;
+cout<<"  _    /   ,    \\/\\_"<<endl;
+cout<<" ((____|    )_- \\ \\_-`"<<endl;
+cout<<"  `-----'`-----``--`"<<endl;
+  system("pause>>nul");
+  return 0;
+}
